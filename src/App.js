ec027e42be68b7e169c508a9987ab4d8d4839a60
saeed <saeed@mary.local>
2023-05-22 18:51:11 +0330

    completing some parts of the Navbar component and creating Search component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import {Footer, Navbar, Routes} from "./component";
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
 
+  const changeTheme = () => {
+    setDarkTheme((prevState) => !prevState);
+  };
+
   return (
     <div className={darkTheme ? "dark" : ""}>
-      <div className=" bg-gray-400 dark:bg-gray-900 dark:text-gray-200 min-h-full">
-        <Navbar />
+      <div className="dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar changeTheme={changeTheme} darkTheme={darkTheme} />
         <Routes />
         <Footer />
       </div>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import Search from "./Search";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = (props) => {
+  const {darkTheme, changeTheme} = props;
+  return (
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
+      <div className="flex justify-between items-center space-x-5 w-screen">
+        <Link to="/">
+          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded-full dark:bg-gray-500 dark:text-gray-900">
+            goggle 🔎
+          </p>
+        </Link>
+        <button
+          type="button"
+          className="text-xl dark:bg-gray-500 bg-white dark:text-gray-900 border rounded-full py-1 px-2 transition-all duration-300 hover:shadow-xl "
+          onClick={changeTheme}>
+          {darkTheme ? "Light 💡" : "Dark 🌙"}
+        </button>
+      </div>
+      <Search />
+    </div>
+  );
 };
 
 export default Navbar;
